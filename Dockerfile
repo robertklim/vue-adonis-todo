65f1506ac22a51b58c5446074fc36ceb0103b2bb
Robert Klim <kontakt.robert.klim@gmail.com>
2018-07-05 14:32:30 +0200

    Add route and controller to create new user

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:9.11.2-slim
 WORKDIR /usr/src/vue-adonis-todo
 RUN apt-get update
 RUN apt-get --yes install git-core
+RUN apt-get --yes install sqlite3
 RUN npm install -g @vue/cli
 RUN npm install -g @adonisjs/cli
 ADD . /usr/src/vue-adonis-todo/
---
+++ server/app/Controllers/Http/UserController.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const User = use('App/Models/User');
+
+class UserController {
+    async register({ request }) {
+        const { email, password } = request.all() // grab request params
+        // console.log(email, password);
+        const user = await User.create({
+            email,
+            password,
+            username: email,
+        });
+        return user;
+    }
+}
+
+module.exports = UserController
