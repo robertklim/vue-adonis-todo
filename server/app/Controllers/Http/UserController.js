60159c9432c9094ab4d3b949f7a71ba42d02ee22
Robert Klim <kontakt.robert.klim@gmail.com>
2018-07-05 14:46:39 +0200

    Add user login

--- server/app/Controllers/Http/UserController.js
+++ server/app/Controllers/Http/UserController.js
@@ -3,15 +3,22 @@
 const User = use('App/Models/User');
 
 class UserController {
+
+    async login ({ request, auth }) {
+        const { email, password } = request.all();
+        const token = await auth.attempt(email, password);
+        return token;
+    }
+
     async register({ request }) {
         const { email, password } = request.all() // grab request params
         // console.log(email, password);
-        const user = await User.create({
+        await User.create({
             email,
             password,
             username: email,
         });
-        return user;
+        return this.login(...arguments);
     }
 }
 
--- server/start/routes.js
+++ server/start/routes.js
@@ -16,6 +16,7 @@
 const Route = use('Route')
 
 Route.group(() => {
+  Route.post('/auth/login', 'UserController.login');
   Route.post('/auth/register', 'UserController.register');
 })
   .prefix('api');
