7cdaf550ee42b54514605cb776f93c7e62da2e72
Robert Klim <kontakt.robert.klim@gmail.com>
2018-07-06 11:47:19 +0200

    Add 'api/projects/:id/tasks' GET (Task index) endpoint

--- server/app/Controllers/Http/TaskController.js
+++ server/app/Controllers/Http/TaskController.js
@@ -1,9 +1,19 @@
 'use strict'
 
+const AuthorizationService = use('App/Services/AuthorizationService');
 const Project = use('App/Models/Project');
 const Task = use('App/Models/Task');
 
 class TaskController {
+
+    async index({ auth, request, params }) {
+        const user = await auth.getUser();
+        const { id } = params;
+        const project = await Project.find(id);
+        AuthorizationService.verifyPermission(project, user);
+        return await project.tasks().fetch();
+    }
+
     async create({ auth, request, params }) {
         const user = await auth.getUser();
         const { description } = request.all();
