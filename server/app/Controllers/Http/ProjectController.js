787f769595e322160388e1709dd3c8b82106c61c
Robert Klim <kontakt.robert.klim@gmail.com>
2018-07-06 09:29:01 +0200

    Add 'api/projects' POST (Projects create) endpoint

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -1,10 +1,23 @@
 'use strict'
 
+const Project = use('App/Models/Project');
+
 class ProjectController {
     // return all projects associated with user
     async index({ auth }) {
         const user = await auth.getUser();
         return await user.projects().fetch();
+    }
+
+    async create({ auth, request }) {
+        const user = await auth.getUser();
+        const { title } = request.all();
+        const project = new Project();
+        project.fill({
+            title,
+        });
+        await user.projects().save(project); // associate project with user
+        return project;
     }
 }
 
--- server/start/routes.js
+++ server/start/routes.js
@@ -16,9 +16,10 @@
 const Route = use('Route')
 
 Route.group(() => {
-  Route.get('projects', 'ProjectController.index').middleware('auth');
-
   Route.post('/auth/login', 'UserController.login');
   Route.post('/auth/register', 'UserController.register');
+
+  Route.get('projects', 'ProjectController.index').middleware('auth');
+  Route.post('projects', 'ProjectController.create').middleware('auth');
 })
   .prefix('api');
