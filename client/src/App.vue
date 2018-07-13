2c5f9e2531353461b78e75d0e3acb07779b66756
Robert Klim <kontakt.robert.klim@gmail.com>
2018-07-13 10:02:18 +0200

    Create 'Toolbar' component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <v-app>
+    <Toolbar/>
+    <div id="app">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view/>
     </div>
-    <router-view/>
-  </div>
+  </v-app>
 </template>
+
+<script>
+import Toolbar from '@/components/Toolbar.vue';
+export default {
+  components: {
+    Toolbar,
+  },
+};
+</script>
+
 
 <style lang="scss">
 #app {
---
+++ client/src/components/Toolbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-toolbar color="green" dark fixed>
+        <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
+        <v-toolbar-title class="mr-4">TODO IN VUE</v-toolbar-title>
+        <v-toolbar-items>
+            <v-btn flat>
+            <v-icon class="pr-2">playlist_add_check</v-icon>
+            PROJECTS
+            </v-btn>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>
+            <v-icon class="pr-2">account_box</v-icon>
+            Register
+            </v-btn>
+            <v-btn flat>
+            <v-icon class="pr-2">fingerprint</v-icon>
+            Login
+            </v-btn>
+            <v-btn flat>
+            <v-icon class="pr-2">exit_to_app</v-icon>
+            Logout
+            </v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style>
+
+</style>
